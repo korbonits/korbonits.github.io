6aab38b65f2ca32fd4c7dac46e6d31e03cbf1fc5
Alexander Korbonits <alexanderkorbonits@Alexanders-MacBook-Air.local>
2026-03-16 21:27:31 -0700

    add RSS feed

---
+++ helpless-houston/src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+
+export async function GET(context) {
+  const posts = (await getCollection('blog')).sort(
+    (a, b) => b.data.date.valueOf() - a.data.date.valueOf()
+  );
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map(post => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      link: `/blog/${post.id}/`,
+    })),
+  });
+}

e0e055a5df648562c4488299de757d2d4d828b21
Alexander Korbonits <alexanderkorbonits@Alexanders-MacBook-Air.local>
2026-03-16 22:17:33 -0700

    github-dark syntax highlighting

--- helpless-houston/astro.config.mjs
+++ helpless-houston/astro.config.mjs
@@ -4,4 +4,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://korbonits.com',
   integrations: [sitemap()],
+  markdown: {
+    shikiConfig: { theme: 'github-dark' },
+  },
 });
